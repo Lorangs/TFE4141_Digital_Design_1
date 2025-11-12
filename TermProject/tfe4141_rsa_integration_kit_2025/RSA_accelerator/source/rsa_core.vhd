6b34ef4df6f87b591f630f68a149f58ddc65797e
susgri <susanne@gripsgaard.no>
2025-11-12 15:35:44 +0100

    removed old signals

--- TermProject/tfe4141_rsa_integration_kit_2025/RSA_accelerator/source/rsa_core.vhd
+++ TermProject/tfe4141_rsa_integration_kit_2025/RSA_accelerator/source/rsa_core.vhd
@@ -64,45 +64,6 @@ entity rsa_core is
 		key_n                   :  in std_logic_vector(C_BLOCK_SIZE-1 downto 0);
 		rsa_status              :  out std_logic_vector(31 downto 0);
 
-
-		-----------------------------------------------------------------------------
-		-- Internal signals for testing. Can be moved to signal interface when testing is done.
-		-----------------------------------------------------------------------------
-		counter				  : inout std_logic_vector(C_BLOCK_SIZE-1 downto 0);
-		
-		
-		-- Control signals from FSM
-		current_state			: inout std_logic_vector(1 downto 0);
-		
-		-- LOAD_NEW_MSG = 00, COUNT_WAIT = 01, COUNT_FIN_PARTIAL = 10, FINISHED = 11
-		msgin_data_reg   : inout std_logic_vector(C_BLOCK_SIZE-1 downto 0);
-		
-		-- Exponentiation module signals
-		exp_valid_out      : inout std_logic;
-		exp_ready_in       : inout std_logic;
-		exp_valid_in       : inout std_logic;
-		exp_ready_out      : inout std_logic;
-		exp_reset_neg      : inout std_logic;
-
-		exp_R_next         : inout std_logic_vector(C_BLOCK_SIZE-1 downto 0);
-		exp_P_next         : inout std_logic_vector(C_BLOCK_SIZE-1 downto 0);
-		exp_e_d            : inout std_logic;				-- exponent bit (LSB first)
-
-
-		---- can be deleted when testing is done ----
-		exp_counter			: inout std_logic_vector(C_BLOCK_SIZE-1 downto 0);
-		exp_current_state	: inout std_logic_vector(1 downto 0);					-- RESET = 00, COUNTING = 01, FINISHED = 10, unused 11
-
-		-- Intermediate and result of R and P. R is to be treated as the resulting ciphertext.
-		result_R          : inout std_logic_vector(C_BLOCK_SIZE-1 downto 0) := (others => '0');
-		result_P          : inout std_logic_vector(C_BLOCK_SIZE-1 downto 0) := (others => '0');
-
-		-- Registers for storing input signals
-		key_e_d_reg      : inout std_logic_vector(C_BLOCK_SIZE-1 downto 0) := (others => '0');
-		key_n_reg        : inout std_logic_vector(C_BLOCK_SIZE-1 downto 0) := (others => '0');
-		n_neg_reg        : inout std_logic_vector(C_BLOCK_SIZE downto 0);
-		msgin_last_reg   : inout std_logic := '0'
-
 	);
 end rsa_core;
 
